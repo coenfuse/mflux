5a89ed2c6d4eee8fdf2506c2d841741b5aa161f5
coenfuse <coefuse@gmail.com>
2022-03-09 00:19:34 +0530

    add (mflux) : mqtt
    Added mqtt module to mflux that will handle all the mqtt related jobs for mflux using the mosquitto library.

---
+++ source/modules/mqtt/include/mqtt/client.h
@@ -0,0 +1,42 @@
+// preprocessor flags
+#pragma once
+
+// standard includes
+// ..
+
+// module includes
+#include "errorcodes/errorcodes.h"
+
+// thirdparty includes
+// ..
+
+// forward references
+// ..
+
+
+
+namespace felidae
+{
+	namespace mqtt
+	{
+		class Client
+		{
+		public:
+			
+			Client(void);
+			~Client(void);
+
+			ERC connect(void);
+			ERC disconnect(void);
+			bool is_connected(void);
+
+			ERC publish(void);
+			ERC subscribe(void);
+			ERC unsubscribe(void);
+
+			ERC start_service(void);
+			ERC stop_service(void);
+			bool is_running(void);
+		};
+	}
+}
