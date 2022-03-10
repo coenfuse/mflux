76fad4e4461184495763a3871a6502a5018c45a3
coenfuse <coefuse@gmail.com>
2022-03-11 00:45:56 +0530

    update (mqtt) : message
    Implemented basic functionality of mqtt message except its serialization and deserialization into and from a string. Those are the planned features that will be later implemented 'if' necessary.
    
    The definition is inlined into the header file as most of the methods are pure getters and setters that do not exceed more than 4-5 lines of code.

--- source/modules/mqtt/include/mqtt/message.h
+++ source/modules/mqtt/include/mqtt/message.h
@@ -29,8 +29,92 @@ namespace felidae
 			Message(void){}
 			~Message(void){}
 
-			std::string dump(){}
-			ERC parse(std::string msg){}
+			inline void set_topic(std::string topic);
+			inline void set_payload(std::string payload);
+			inline void set_qos(uint8_t qos);
+			inline void set_to_retain(bool retain_status);
+
+			inline std::string get_topic(void);
+			inline std::string get_payload(void);
+			inline uint8_t get_qos(void);
+			inline bool get_to_retain(void);
+
+			inline std::string dump();
+			inline ERC parse(std::string msg);
+
+		private:
+
+			std::string m_topic;
+			std::string m_payload;
+			uint8_t m_qos = 0;
+			bool m_to_retain = false;
 		};
+
+
+
+		// Definitions
+		void Message::set_topic(std::string topic)
+		{
+			if (!topic.empty())
+				m_topic = topic;
+		}
+
+		void Message::set_payload(std::string payload)
+		{
+			m_payload = payload;
+		}
+
+		void Message::set_qos(uint8_t qos)
+		{
+			const uint8_t MAX_QOS = 2;
+
+			if (qos <= MAX_QOS)
+				m_qos = qos;
+			else
+				m_qos = MAX_QOS;
+		}
+
+		void Message::set_to_retain(bool retain_status)
+		{
+			m_to_retain = retain_status;
+		}
+
+		std::string Message::get_topic(void)
+		{
+			return m_topic;
+		}
+
+		std::string Message::get_payload(void)
+		{
+			return m_payload;
+		}
+
+		uint8_t Message::get_qos(void)
+		{
+			return m_qos;
+		}
+
+		bool Message::get_to_retain(void)
+		{
+			return m_to_retain;
+		}
+
+		std::string Message::dump(void)
+		{
+			auto mqtt_msg_dump = "";
+
+			// TODO : MQTT msg serialization
+
+			return mqtt_msg_dump;
+		}
+
+		ERC Message::parse(std::string msg)
+		{
+			auto status = ERC::SUCCESS;
+
+			// TODO : MQTT msg deserialization
+
+			return status;
+		}
 	}
 }
