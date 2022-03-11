3b660faeb3f76cc985e6ffc5cbdef216a6957d65
coenfuse <coefuse@gmail.com>
2022-03-11 18:05:32 +0530

    update (mqtt) : mqtt service
    Implemented basic mqtt service that is running on a separate thread and pushing random data to buffer.

--- source/modules/mqtt/include/mqtt/client.h
+++ source/modules/mqtt/include/mqtt/client.h
@@ -42,6 +42,19 @@ namespace felidae
 			ERC start_service(std::shared_ptr<Configurator> p_config, std::shared_ptr<MemDB> p_buffer);
 			ERC stop_service(void);
 			bool is_running(void);
+
+		private:
+
+			static void s_service_wrapper(void* instance);
+			void i_actual_job(void);
+
+		private:
+
+			std::atomic_bool m_signalled_stop = true;
+			std::thread m_worker;
+
+			std::shared_ptr<Configurator> m_pConfig;
+			std::shared_ptr<MemDB> m_pBuffer;
 		};
 	}
 }
--- source/modules/mqtt/src/client.cpp
+++ source/modules/mqtt/src/client.cpp
@@ -84,7 +84,22 @@ namespace felidae
 		{
 			auto status = ERC::SUCCESS;
 
-			// ..
+			if (!this->is_running())
+			{
+				m_pConfig = p_config;
+				m_pBuffer = p_buffer;
+
+				if ((m_pConfig == nullptr) || (m_pBuffer == nullptr))
+					status = ERC::NULLPTR_RECV;
+
+				if (status == ERC::SUCCESS)
+					m_worker = std::thread(s_service_wrapper, this);
+
+				if (this->is_running())
+					status = ERC::SUCCESS;
+
+				fmt::print("MQTT service started\n");
+			}
 
 			return status;
 		}
@@ -93,18 +108,59 @@ namespace felidae
 		{
 			auto status = ERC::SUCCESS;
 
-			// ..
+			if (this->is_running())
+			{
+				m_signalled_stop.exchange(true);
+				m_worker.join();
+			}
 
 			return status;
 		}
 
 		bool Client::is_running(void)
 		{
-			auto status = true;
+			return m_worker.joinable();
+		}
 
-			// ..
 
-			return status;
+
+		// PRIVATE DEFINITIONS
+		// ------------------------------------------------------
+
+		void Client::i_actual_job(void)
+		{
+			auto status = ERC::SUCCESS;
+
+			auto buffer_name = Configurator::get_mqtt_inbox_name();
+			Message mqtt_msg;
+			DBitem dbitem;
+
+			m_signalled_stop.exchange(false);
+
+			while (!m_signalled_stop)
+			{
+				srand(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+				mqtt_msg.set_topic("BASSAI");
+				mqtt_msg.set_qos(rand() % 3);
+				mqtt_msg.set_to_retain(rand() % 2);
+				mqtt_msg.set_payload(fmt::format("MQTT msg ID {}\n", rand() % 99));
+
+				dbitem.set<Message>(mqtt_msg);
+
+				m_pBuffer->push(buffer_name, dbitem);
+				
+				fmt::print("Sent {} {}", mqtt_msg.get_topic(), mqtt_msg.get_payload());
+
+				// Take a break for a while
+				std::this_thread::sleep_for(std::chrono::seconds(3));
+			}
+		}
+		
+		void Client::s_service_wrapper(void* instance)
+		{
+			Client* self_instance = (Client*)instance;
+			self_instance->i_actual_job();						// This thread blocks here
 		}
 	}
 }
