154ff2b14fba473f9639255368dc141f967413e4
coenfuse <coefuse@gmail.com>
2022-03-11 17:27:15 +0530

    update (influx) : influx service
    Implemented basic influx service that is running on a separate thread.

--- source/modules/influx/include/influx/client.h
+++ source/modules/influx/include/influx/client.h
@@ -2,7 +2,9 @@
 #pragma once
 
 // standard includes
+#include <atomic>
 #include <memory>
+#include <thread>
 
 // internal includes
 // ..
@@ -43,6 +45,19 @@ namespace felidae
 			ERC start_service(std::shared_ptr<Configurator> p_config, std::shared_ptr<MemDB> p_buffer);
 			ERC stop_service(void);
 			bool is_running(void);
+
+		private:
+
+			static void s_service_wrapper(void* instance);
+			void m_actual_job(void);
+
+		private:
+
+			std::atomic_bool m_signalled_stop = true;
+			std::thread m_worker;
+
+			std::shared_ptr<Configurator> m_pConfig;
+			std::shared_ptr<MemDB> m_pBuffer;
 		};
 	}
 }
