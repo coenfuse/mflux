98d396e2f2321cfccc6966e275af0435858f2c88
coenfuse <65540834+coenfuse@users.noreply.github.com>
2022-03-19 17:16:09 +0530

    update docs (minix)

--- source/modules/minix/src/minix.cpp
+++ source/modules/minix/src/minix.cpp
@@ -1,15 +1,19 @@
 // standard includes
 // ..
 
+
 // internal includes
 #include "minix/minix.h"
+
 
 // module includes
 #include "influx/message.h"
 #include "mqtt/message.h"
 
+
 // thirdparty includes
 // ..
+
 
 // forward references
 // ..
@@ -37,7 +41,9 @@ namespace felidae
         ERC get_mqtt_msg_from(influx::Message this_msg, mqtt::Message& into_this)
         {
             auto status = ERC::SUCCESS;
+            
             // ..
+
             return status;
         }
     }
